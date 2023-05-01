78fe9d0601781b3868cff8a5a250b2fcc5a2b3cb
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-05-01 22:19:29 +0300

    Initial commit before nextAuth

---
+++ components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import style from "@/styles/Sidebar.module.css";
+
+export default function Sidebar() {
+
+    return (
+        <div className={style.main}>
+            <div className={style.locations}>
+                <div className={style.location}>
+                    Home
+                </div>
+                <div className={style.location}>
+                    Search
+                </div>
+                <div className={style.location}>
+                    Your Library
+                </div>
+                <div className={style.location}>
+                    Create Playlist
+                </div>
+                <div className={style.location}>
+                    Liked Songs
+                </div>
+                <div className={style.location}>
+                    Your Episodes
+                </div>
+            </div>
+        </div>
+    );
+};
